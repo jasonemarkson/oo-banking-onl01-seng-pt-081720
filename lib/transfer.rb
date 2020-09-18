85ee5e67368b6820a8e0febec967ccdef3ff1b7c
Jason Markson <jasonmarkson76@gmail.com>
2020-09-18 00:30:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,10 @@ class Transfer
   end
   
   def reverse_transfer
-    binding.pry 
+    if status == "complete"
+      sender.balance += amount 
+      receiver.balance -= amount 
+    end
   end
   
 end
