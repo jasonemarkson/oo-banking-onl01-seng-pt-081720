7b38d3ccea25ac677dfbdf33a16bcf19aef862cd
Jason Markson <jasonmarkson76@gmail.com>
2020-09-18 00:22:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,6 @@ class Transfer
   end
   
   def execute_transaction
-    #binding.pry 
     if valid? && self.status == "pending" && sender.balance > amount 
       sender.balance -= amount
       receiver.balance += amount
